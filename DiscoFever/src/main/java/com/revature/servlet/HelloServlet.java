d74d2d115bd2ba7924707e0d205d45f35dba0565
Inioluwa OluAlakija <inioluwa4@gmail.com>
2020-03-05 14:44:07 -0500

    sd

--- DiscoFever/src/main/java/com/revature/servlet/HelloServlet.java
+++ DiscoFever/src/main/java/com/revature/servlet/HelloServlet.java
@@ -12,7 +12,7 @@ public class HelloServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		resp.getWriter().write("I have that disco fever, do you?.");
+		resp.getWriter().write("I have that disco fever, do you? HaHA.");
 	}
 	
 
